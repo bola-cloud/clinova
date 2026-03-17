c7219d6479ba994e3c51d346f388f3a5693b4201
bola-cloud <bola.ishak41@gmail.com>
2026-03-17 03:27:14 +0200

    post method and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
                 'name' => 'System Admin',
                 'password' => bcrypt('password'),
                 'role' => 'admin',
+                'email_verified_at' => now(),
                 'subscription_active' => true,
             ]
         );
@@ -33,6 +34,7 @@ class DatabaseSeeder extends Seeder
                 'name' => 'Dr. Ahmed Ali',
                 'password' => bcrypt('password'),
                 'role' => 'doctor',
+                'email_verified_at' => now(),
                 'subscription_active' => true,
                 'subscription_expires_at' => now()->addYear(),
             ]
@@ -46,6 +48,7 @@ class DatabaseSeeder extends Seeder
                 'password' => bcrypt('password'),
                 'role' => 'secretary',
                 'doctor_id' => $doctor->id,
+                'email_verified_at' => now(),
                 'subscription_active' => true,
             ]
         );
